8ec42f08536805bfc53c403be25044f4260497da
Kwe <Kweayon@gmail.com>
2024-07-07 23:22:07 -0500

    feat: services

---
+++ lib/shared/services/services.dart
@@ -0,0 +1,6 @@
+import 'package:mvvm_example_app/shared/services/quotes_service.dart';
+import 'package:mvvm_example_app/shared/services/toast_service.dart';
+
+QuotesService get quotesService => QuotesService();
+
+ToastService get toastService => ToastService();
