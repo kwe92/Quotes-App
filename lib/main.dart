aa051bd716fbbb5d97c5f0951a853bb099a95f13
Kwe <Kweayon@gmail.com>
2024-07-09 15:57:31 -0500

    feat: moved QuotesView creation to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_example_app/app/constants.dart';
 import 'package:mvvm_example_app/features/home/ui/quotes_view.dart';
+import 'package:mvvm_example_app/features/home/ui/quotes_view_model.dart';
+import 'package:mvvm_example_app/shared/repositories/quotes_repo.dart';
+import 'package:mvvm_example_app/shared/services/services.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
@@ -15,5 +19,8 @@ class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) => const QuotesView();
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => QuotesViewModel(QuotesRepository(quotesService), toastService),
+        builder: (context, _) => const QuotesView(),
+      );
 }
