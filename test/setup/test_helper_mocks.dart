dee26b3270c735577e3e94d78be27247d4f03abe
Kwe <Kweayon@gmail.com>
2024-07-09 15:56:58 -0500

    feat: added getToastServiceMock to TestHelperMocks implementation

--- test/setup/test_helper_mocks.dart
+++ test/setup/test_helper_mocks.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mvvm_example_app/app/constants.dart';
+import 'package:mvvm_example_app/shared/services/toast_service.dart';
+import 'package:provider/provider.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:mvvm_example_app/shared/repositories/quotes_repo.dart';
 import 'package:mvvm_example_app/shared/services/quotes_service.dart';
@@ -7,6 +12,8 @@ import 'test_data.dart';
 class MockQuotesService extends Mock implements QuotesService {}
 
 class MockQuotesRepository extends Mock implements QuotesRepository {}
+
+class MockToastService extends Mock implements ToastService {}
 
 class TestHelperMocks {
   TestHelperMocks._();
@@ -33,5 +40,58 @@ class TestHelperMocks {
 
     // return mock
     return repo;
+  }
+
+  static MockToastService getToastServiceMock() {
+    // instantiate mock
+    final MockToastService service = MockToastService();
+
+    // mock functions
+    when(() => service.showSnackBar(any())).thenReturn(null);
+
+    // return mock
+    return service;
+  }
+}
+
+Future<void> pumpWidget<T extends ChangeNotifier>(WidgetTester tester, Widget view, [T? viewModel]) async {
+  await tester.pumpWidget(
+    TestingWrapper(
+      view: view,
+      viewModel: viewModel,
+    ),
+  );
+}
+
+// Future<void> pumpWidget<T extends ChangeNotifier>(WidgetTester tester, Widget view, [T? viewModel]) async {
+//   await tester.pumpWidget(
+//     TestingWrapper(
+//       view: view,
+//       viewModel: viewModel,
+//     ),
+//   );
+// }
+
+class TestingWrapper<T extends ChangeNotifier> extends StatelessWidget {
+  final Widget view;
+  final T? viewModel;
+
+  const TestingWrapper({
+    required this.view,
+    this.viewModel,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      key: WidgetKey.rootScaffoldMessengerKey,
+      home: viewModel != null
+          ? ChangeNotifierProvider(
+              create: (context) => viewModel,
+              builder: (context, child) => view,
+            )
+          : view,
+    );
   }
 }
