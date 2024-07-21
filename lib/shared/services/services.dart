37098bf296711c723d492e8b24e73e975a378881
Kwe <Kweayon@gmail.com>
2024-07-21 01:02:13 -0500

    feat: added client service singleton and injected client into quotes service

--- lib/shared/services/services.dart
+++ lib/shared/services/services.dart
@@ -1,6 +1,10 @@
+import 'package:http/http.dart' as http;
+import 'package:mvvm_example_app/shared/services/http_client_service.dart';
 import 'package:mvvm_example_app/shared/services/quotes_service.dart';
 import 'package:mvvm_example_app/shared/services/toast_service.dart';
 
-QuotesService get quotesService => QuotesService();
+HttpClientService get httpClientService => HttpClientService(http.Client());
+
+QuotesService get quotesService => QuotesService(httpClientService.client);
 
 ToastService get toastService => ToastService();
